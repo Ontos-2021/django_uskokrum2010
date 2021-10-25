63f7e485c0f21993667bed70325d333bc490f38e
_Ontos_ <josemercado.musica@gmail.com>
2021-10-25 18:26:27 -0300

    Terminé de codificar los modelos

--- Universidad/Apps/GestionAcademica/models.py
+++ Universidad/Apps/GestionAcademica/models.py
@@ -21,3 +21,22 @@ class Alumno(models.Model):
 
     def __str__(self):
         return self.NombreCompleto()
+
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=50)
+    creditos = models.PositiveSmallIntegerField()
+    estado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return "{0} ({1})".format(self.nombre, self.creditos)
+
+
+class Matricula(models.Model):
+    alumno = models.ForeignKey(Alumno, null=False, blank=False, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, null=False, blank=False, on_delete=models.CASCADE)
+    FechaMatricula = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        cadena = "{0} => {1}"
+        return cadena.format(self.alumno, self.curso.nombre)
